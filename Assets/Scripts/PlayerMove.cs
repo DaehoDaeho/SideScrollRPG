14bc63fa20f70522656a2d43384cc0d5cc8ae220
DaehoDaeho <relayer@naver.com>
2025-07-30 15:31:28 +0900

    .

--- Assets/Scripts/PlayerMove.cs
+++ Assets/Scripts/PlayerMove.cs
@@ -1,3 +1,4 @@
+using TMPro;
 using UnityEngine;
 
 public class PlayerMove : MonoBehaviour
@@ -6,6 +7,8 @@ public class PlayerMove : MonoBehaviour
     public float jumpPower = 5.0f;
     private bool isGrounded = true;
     private int jumpCount = 0;
+    public GameObject objText;
+    public TextMeshProUGUI textMessage;
 
     // Update is called once per frame
     void Update()
@@ -15,13 +18,18 @@ public class PlayerMove : MonoBehaviour
 
         if(Input.GetKeyDown(KeyCode.Space) == true && (isGrounded == true || jumpCount < 2))
         {
-            Rigidbody2D rb = gameObject.GetComponent<Rigidbody2D>();
-            if(rb != null)
-            {
-                rb.AddForce(Vector2.up * jumpPower, ForceMode2D.Impulse);
-                isGrounded = false;
-                ++jumpCount;
-            }
+            Jump();
+        }
+    }
+
+    void Jump()
+    {
+        Rigidbody2D rb = gameObject.GetComponent<Rigidbody2D>();
+        if (rb != null)
+        {
+            rb.AddForce(Vector2.up * jumpPower, ForceMode2D.Impulse);
+            isGrounded = false;
+            ++jumpCount;
         }
     }
 
@@ -29,5 +37,26 @@ public class PlayerMove : MonoBehaviour
     {
         isGrounded = true;
         jumpCount = 0;
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if(collision.gameObject.tag == "NPC")
+        {
+            NPC npc = collision.gameObject.GetComponent<NPC>();
+            if(npc != null)
+            {
+                textMessage.text = npc.message;
+            }
+            objText.SetActive(true);
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if(collision.gameObject.tag == "NPC")
+        {
+            objText.SetActive(false);
+        }
     }
 }
