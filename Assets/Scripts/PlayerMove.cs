03e8887131a589a995c12eec3835e122ff389ba6
DaehoDaeho <relayer@naver.com>
2025-08-20 15:40:08 +0900

    .

--- Assets/Scripts/PlayerMove.cs
+++ Assets/Scripts/PlayerMove.cs
@@ -5,6 +5,17 @@ using System.Collections.Generic;
 
 public class PlayerMove : MonoBehaviour
 {
+    public Animator animator;
+
+    //public Sprite[] idleSprites;
+    //public Sprite[] moveSprites;
+
+    int state = 0;  // 0 : 대기, 1 : 이동.
+    public SpriteRenderer sr;
+    //int frame = 0;
+    //float timer = 0.0f;
+    //public float frameRate = 0.15f;
+
     public float speed = 5.0f;
     public float jumpPower = 5.0f;
     private bool isGrounded = true;
@@ -30,12 +41,33 @@ public class PlayerMove : MonoBehaviour
     void Update()
     {
         float h = Input.GetAxisRaw("Horizontal");
+        bool move = false;
+
+        if(h != 0.0f)
+        {
+            state = 1;
+            move = true;
+        }
+        else
+        {
+            state = 0;
+        }
+
         transform.position += Vector3.right * h * speed * Time.deltaTime;
 
         if(Input.GetKeyDown(KeyCode.Space) == true && (isGrounded == true || jumpCount < 2))
         {
             Jump();
         }
+
+        //timer += Time.deltaTime;
+        //if(timer >= frameRate)
+        //{
+        //    timer = 0.0f;
+        //    PlayAnimation();
+        //}
+
+        animator.SetBool("Move", move);
     }
 
     void Jump()
@@ -79,5 +111,17 @@ public class PlayerMove : MonoBehaviour
         {
             objText.SetActive(false);
         }
+    }
+
+    void PlayAnimation()
+    {
+        //Sprite[] curArr = idleSprites;
+        //if(state == 1)  // 현재 이동상태면.
+        //{
+        //    curArr = moveSprites;
+        //}
+
+        //frame = (frame + 1) % curArr.Length;
+        //sr.sprite = curArr[frame];  // 현재 동작의 현재 프레임에 해당하는 스프라이트를 넣어준다.
     }
 }
